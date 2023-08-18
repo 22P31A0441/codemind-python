ff39b298a0c3c4a4d44973d19bb9c62bf7b41a92
22P31A0441 <119719043+22P31A0441@users.noreply.github.com>
2023-08-18 15:59:23 +0530

    a,b,c=map(int,input().split()) if a>b and a>c:     if b>c:         print(a+b)     else:         print(a+c) if b>a and b>c:     if c>b:         print(b+c)     else:         print(b+a) if c>a and c>b:     if a>b:         print(a+c)     else:         print(c+b)

--- Hermoine_and_Spells.py
+++ Hermoine_and_Spells.py
@@ -1,1 +1,16 @@
-a,b
+a,b,c=map(int,input().split())
+if a>b and a>c:
+    if b>c:
+        print(a+b)
+    else:
+        print(a+c)
+if b>a and b>c:
+    if c>b:
+        print(b+c)
+    else:
+        print(b+a)
+if c>a and c>b:
+    if a>b:
+        print(a+c)
+    else:
+        print(c+b)
